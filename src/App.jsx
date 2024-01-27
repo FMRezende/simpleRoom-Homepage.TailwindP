f73ae0491e58373752e58ed3ba6af9f4138a1c7f
Felipe Rezende <f.zende@gmail.com>
2024-01-27 15:06:07 +0100

    done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import NavBar from "./components/menu/NavBar";
+import MainHome from "./components/home/MainHome";
+import HeaderHome from "./components/home/HeaderHome";
+
+const App = () => {
+    return (
+        <>
+            <NavBar />
+            <HeaderHome />
+            <MainHome />
+        </>
+    );
+};
+
+export default App;
